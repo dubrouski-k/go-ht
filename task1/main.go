33628cf347540307d880e4f799fb0a7ae66aacfd
Odminko <deadhead@tut.by>
2019-03-06 14:29:59 +0300

    task1 done

--- task1/main.go
+++ task1/main.go
@@ -1,6 +1,17 @@
 package main
 
 // Specify Filter function here
+func Filter(arr []int, predicate func(elem, _ int)bool) []int{
+	var result []int
+	for _, value := range arr{
+		if predicate(value, value) {
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+
 
 func main() {
 
